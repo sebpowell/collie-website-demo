e1144ecc85f6777cb392609817e7dd487ddee3a3
Sebastien Powell <sebpowell@users.noreply.github.com>
2024-10-18 07:51:25 +0100

    Removed Posthog

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,16 +2,13 @@
 import { ReactNode } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "react-wrap-balancer";
-import { CSPostHogProvider } from "@/packages/contexts/posthog.context";
 
 export const queryClient = new QueryClient();
 
 export const Providers = ({ children }: { children: ReactNode }) => {
   return (
-    <CSPostHogProvider>
-      <QueryClientProvider client={queryClient}>
-        <Provider>{children}</Provider>
-      </QueryClientProvider>
-    </CSPostHogProvider>
+    <QueryClientProvider client={queryClient}>
+      <Provider>{children}</Provider>
+    </QueryClientProvider>
   );
 };
